df3533ec8b6f694c5bb67446c368f4db451cccb7
wxh <wxh@wxh-asus>
2021-02-02 20:56:19 +0800

    auto和const更新

--- eg_auto/main.cpp
+++ eg_auto/main.cpp
@@ -1,124 +1,110 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-using namespace std;
+//auto的自动类型推断发生在编译期，所以使用auto并不会造成程序运行时效率的降低,编译的时候，就会把auto变量转换为相应的变量类型。
 
-enum DATA_TYPE
-{
-    DATA_NULL = 0,
-    DATA_CHAR,
-    DATA_UINT,
-    DATA_UINT8,
-    DATA_UINT16,
-    DATA_UINT32,
-    DATA_INT,
-    DATA_FLOAT,
-    DATA_DOUBLE,
-};
-
-class data_value
-{
-public:
-    data_value()
-    {
-        data_type = DATA_TYPE::DATA_INT;
-    }
-    data_value(int type)
-    {
-        data_type = type;
-    }
-    void change_type(int type)
-    {
-        data_type = type;
-    }
-    void operator=(uint8_t state_)
-    {
-        data_type = DATA_TYPE::DATA_UINT8;
-        value_8 = state_;
-    }
-    void operator=(uint16_t state_)
-    {
-        data_type = DATA_TYPE::DATA_UINT16;
-        value_16 = state_;
-    }
-    void operator=(uint32_t state_)
-    {
-        data_type = DATA_TYPE::DATA_UINT32;
-        value_32 = state_;
-    }
-    void operator=(int state_)
-    {
-        data_type = DATA_TYPE::DATA_INT;
-        value_i = state_;
-    }
-    void operator=(float state_)
-    {
-        data_type = DATA_TYPE::DATA_FLOAT;
-        value_f = state_;
-    }
-    void operator=(double state_)
-    {
-        std::cout << "........" << std::endl;
-        data_type = DATA_TYPE::DATA_DOUBLE;
-        value_d = state_;
-    }
-    void value(uint8_t &value)
-    {
-        value = value_8;
-    }
-    void value(uint16_t &value)
-    {
-        value = value_16;
-    }
-    void value(uint32_t &value)
-    {
-        value = value_32;
-    }
-    void value(int &value)
-    {
-        value = value_i;
-    }
-    void value(float &value)
-    {
-        value = value_f;
-    }
-    void value(double &value)
-    {
-        value = value_d;
-    }
-
-private:
-    uint8_t value_8;
-    uint16_t value_16;
-    uint32_t value_32;
-    int value_i;
-    float value_f;
-    double value_d;
-    int data_type;
-};
+//注1：auto声明的变量必须要初始化，否则编译器不能判断变量的类型。
+//注2：auto不能被声明为返回值，auto不能作为形参，auto不能被修饰为模板参数
 
 int main()
 {
-    data_value data_0;
-    data_value data_1(DATA_TYPE::DATA_FLOAT);
-    data_0 = 10;
-    float a = 10.254;
-    data_1 = a;
-    int v_a;
-    data_0.value(v_a);
-    float v_f, v_f2;
-    data_1.value(v_f);
-    data_value data_2 = data_1;
-    data_2.value(v_f2);
+    // 数据类型的赋值
+    // double a = 10;
+    // auto b = a;
+    // std::cout << "b'type:" << typeid(b).name() << std::endl
+    //           << "b'value:" << b << std::endl;
+    // int c;
+    // auto d = c;
+    // std::cout << "d'type:" << typeid(d).name() << std::endl
+    //           << "d'value:" << d << std::endl;
 
-    std::cout << "data_0 value:" << v_a << std::endl;
-    std::cout << "data_1 value:" << v_f << std::endl;
-    std::cout << "data_2 value:" << v_f2 << std::endl;
-    double b = 1.24578454;
-    data_0 = b;
+    //----------------------//
+    // int a = 10;
+    // auto b = a;
+    // auto c = b;
+    // std::cout << "b'type:" << typeid(b).name() << std::endl
+    //           << "b'value:" << b << std::endl;
+    // std::cout << "c'type:" << typeid(c).name() << std::endl
+    //           << "c'value:" << c << std::endl;
 
-    double v_d;
-    data_0.value(v_d);
-    std::cout << "data_0 value:" << v_d << std::endl;
+    //auto代替复杂的数据类型
+    // std::vector<int> a_v;
+    // int i;
+    // for (i = 0; i < 10; i++)
+    //     a_v.push_back(i);
+    // std::cout << "a_temp:";
+    // for (auto a_temp = a_v.begin(); a_temp != a_v.end(); a_temp++)
+    //     std::cout << "num:" << *a_temp << ",address:" << &a_temp << ",a_v address:" << a_temp.base() << std::endl;
+
+    //decltype和auto的配合使用
+    // int a = 10;
+    // auto b = a;
+    // decltype(b) c = a;
+    // std::cout << "b'type:" << typeid(b).name() << std::endl
+    //           << "b'value:" << b << std::endl;
+    // std::cout << "c'type:" << typeid(c).name() << std::endl
+    //           << "c'value:" << c << std::endl;
+
+    //auto自动推导类型时的规则
+    //如果初始化表达式是引用，则去除引用语义。
+    // int a = 10;
+    // int &b = a;
+    // auto c = b; //这里的auto代表int
+    // auto &d = b;
+    // c = 20;
+    // std::cout << "a'type:" << typeid(a).name() << std::endl
+    //           << "a'value:" << a << std::endl;
+    // std::cout << "b'type:" << typeid(b).name() << std::endl
+    //           << "b'value:" << b << std::endl;
+    // std::cout << "c'type:" << typeid(c).name() << std::endl
+    //           << "c'value:" << c << std::endl;
+    // d = 30;
+    // std::cout << "a'type:" << typeid(a).name() << std::endl
+    //           << "a'value:" << a << std::endl;
+    // std::cout << "b'type:" << typeid(b).name() << std::endl
+    //           << "b'value:" << b << std::endl;
+    // std::cout << "d'type:" << typeid(d).name() << std::endl
+    //           << "d'value:" << d << std::endl;
+
+    //如果初始化表达式为const或volatile（或者两者兼有），则除去const/volatile语义
+    // const int a = 10;
+    // auto b = a;       //b的类型为int而非const int（去除const）
+    // const auto c = a; //此时c的类型为const int
+    // b = 100;          //合法
+    // // c1 = 100;          //非法,无法修改
+
+    //如果auto关键字带上&号，则不去除const语意。
+    // const int a = 10;
+    // auto &b = a; //b的类型为const int
+    // b = 100;     //非法,无法修改
+
+    //初始化表达式为数组时，auto关键字推导类型为指针。
+    int a[3] = {1, 2, 3};
+    auto b = a;
+    std::cout << "b'type:" << typeid(b).name() << std::endl
+              << "b'value:" << b << std::endl;
+    std::cout << "b[0]'type:" << typeid(b[0]).name() << std::endl
+              << "b[0]'value:" << b[0] << std::endl;
+    //若表达式为数组且auto带上&，则推导类型为数组类型。
+    auto &c = a;
+    std::cout << "c'type:" << typeid(c).name() << std::endl
+              << "c'value:" << c << std::endl;
+    std::cout << "c'type:" << typeid(c[0]).name() << std::endl
+              << "c'value:" << c[0] << std::endl;
+
+    //易错写法
+    // auto a = 10, b = 20, c = 30; //正确，数据类型统一，都是int型
+    // // auto a = 10, b = 20, c = 30.24;//错误，变量数据类型不统一
+    // std::cout << "a'type:" << typeid(a).name() << std::endl
+    //           << "a'value:" << a << std::endl;
+    // std::cout << "b'type:" << typeid(b).name() << std::endl
+    //           << "b'value:" << b << std::endl;
+    // std::cout << "c'type:" << typeid(c).name() << std::endl
+    //           << "c'value:" << c << std::endl;
+
+    // std::cout << sizeof(auto) << std::endl;        //错误
+    // std::cout << typeid(auto).name() << std::endl; //错误
+
     return 0;
 }
--- eg_const/main.cpp
+++ eg_const/main.cpp
@@ -1,46 +1,94 @@
 
 #include <iostream>
 
-void first_func(char *str_c)
+//const和define的区别：预编译指令只是对值进行简单的替换，不能进行类型检查
+
+//优点：1、可以保护被修饰的东西，防止意外修改，增强程序的健壮性
+//     2、编译器通常不为普通const常量分配存储空间，而是将它们保存在符号表中，这使得它成为一个编译期间的常量，没有了存储与读内存的操作，使得它的效率也很高。
+
+//作为全局变量防止被修改
+const int count = 10;
+
+//作为函数参数和返回值使用
+const char *test(const char *str, const int num)
 {
-    str_c[5] = 'x';
-}
-void func(char *str, const char *str_c)
-{
-    str[5] = {'a'};
-    char *str_temp = (char *)str_c;
-    first_func(str_temp);
-    // str_c[5] = {'4'};  //错误，该值不可被修改
-    std::cout << "function str:" << str << std::endl;
-    std::cout << "function str_temp:" << str_temp << std::endl;
-    std::cout << "function str_c:" << str_c << std::endl;
+    std::cout << "this num:" << num << " is can not change" << std::endl;
+
+    char *temp = "it is temp";
+    str = temp;
+    return str;
 }
 
 int main()
 {
-    const int num_1 = 5;
-    int const num_2 = 50; //两种方式都表明定义的变量的值不能被修改
+    //不可修改常量
+    // const int num_1 = 3;
+    // int const num_2 = 3; //两种方式都表明定义的变量的值不能被修改
+    // // num_1=20;num_2=10;  //错误
+    // const char *str = "abcdef";
+    // str[3] = 'q';//错误
+    // int num = 3;
+    // char buffer[num] = {'0', '1', '2'};
+    // std::cout << "buffer size:" << sizeof(buffer) << std::endl; //size为3
+    // std::cout << "buffer:" << buffer << std::endl;              //超出3个字符后还有数据输出：012U�
+    // char buff[num_1] = {'0', '1', '2'};
+    // std::cout << "buff:" << sizeof(buff) << std::endl; //size为3
+    // std::cout << "buff:" << buff << std::endl;         //输出也是3个数据：012
 
-    // num_1=20;num_2=10;  //错误
-    int num = 3;
-    // char buffer[num] = {'0', '1', '2'}; //错误用法
-    char buff[num_1] = {'0', '1', '2', '3', '4'};
-    std::cout << "buff:" << buff << std::endl;
+    // //常量指针:不能通过这个指针改变变量的值，但是还是可以通过其他的引用来改变变量的值的;
+    // int temp = 10;
+    // int const *num = &temp;
+    // // const int *num = &temp;//两种定义都可以
+    // // *num = 20;//错误
+    // std::cout << "num ptr:" << num << std::endl;
+    // std::cout << "num:" << *num << std::endl;
+    // temp = 20;
+    // std::cout << "num ptr:" << num << std::endl;
+    // std::cout << "num:" << *num << std::endl;
+    // //常量指针:指向的值不能改变，但是这并不是意味着指针本身不能改变，常量指针可以指向其他的地址。
+    // int other = 20;
+    // num = &other;
+    // std::cout << "num ptr:" << num << std::endl;
+    // std::cout << "num:" << *num << std::endl;
 
-    const int *ptr_1;
-    int const *ptr_2;
-    int a = 5;
-    int b = 10;
-    ptr_1 = &a; //可以修改指针的值
-    // *ptr_2 = b;  //不可修改指针所指向的值
+    //指针常量:指针本身是个常量，不能在指向其他的地址,但是地址中保存的数值是可以改变的，可以通过其他指向该地址的指针来修改
+    // int temp = 10;
+    // int *const num = &temp;
+    // int other = 30;
+    // // num = &other;//错误
+    // temp = 20;
+    // std::cout << "num ptr:" << num << std::endl;
+    // std::cout << "num:" << *num << std::endl;
+    // int *other_num = &temp;
+    // *other_num = 30;
+    // std::cout << "num ptr:" << num << std::endl;
+    // std::cout << "num:" << *num << std::endl;
 
-    int *const ptr_3 = &a; //必须进行初始化
-    int c = 6;
-    // ptr_3 = &b;  //错误，不可以修改指针的值
-    *ptr_3 = c; //正确，可以修改指针所值向的值
+    // //指向常量的常指针:指针指向的位置不能改变并且也不能通过这个指针改变变量的值，但是依然可以通过其他的普通指针改变变量的值
+    // int temp = 10;
+    // const int *const num = &temp;
+    // // int const *const num = &temp;//两种定义都可以
+    // std::cout << "num ptr:" << num << std::endl;
+    // std::cout << "num:" << *num << std::endl;
+    // int *other = &temp;
+    // *other = 20;
+    // std::cout << "num ptr:" << num << std::endl;
+    // std::cout << "num:" << *num << std::endl;
 
-    char str[5] = {'0', '1', '2', '3', '4'};
-    char str_c[5] = {'0', '1', '2', '3', '4'};
-    func(str, str_c);
+    // //作为函数参数与返回值使用
+    // const char *para_str = "it is a parament";
+    // const int num = 10;
+    // const char *ret_str = test(para_str, num);
+    // std::cout << "ret_str:" << ret_str << std::endl;
+
+    char *p1 = "abc";
+    char *p2 = "abc";
+    const char *p3 = "abc";
+    const char *p4 = "abc";
+
+    std::cout << "p1:" << &p1[0] << std::endl;
+    std::cout << "p2:" << &p2[0] << std::endl;
+    std::cout << "p3:" << &p3[0] << std::endl;
+    std::cout << "p4:" << &p4[0] << std::endl;
     return 0;
 }
